f4a2dfddf1a30db4d16a7080ee639caded69b359
Vincent Laptop <vwong140@my.bcit.ca>
2022-05-12 20:42:36 -0700

    Attempted to do flask

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask,  render_template
+from flask import Flask,  render_template, request
+import random
+import sys
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods =['POST', 'GET'])
 def home():
     return render_template("index.html")
+        
+def submit():
+    usrinput = request.form['usrinput']
+    list = []
+    list.append(usrinput)
+    return list
+
+def random():
+    random_value = random.choice(list)
+    return render_template("index.html", random_value)
+    
 
 if __name__ == "__main__":
     app.run()
