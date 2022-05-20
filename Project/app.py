a7e2738b5a206e4a0008c18077fef4539c4a3393
Ajay S <ajaysahota132@gmail.com>
2022-05-19 18:29:42 -0700

    added vincent version

---
+++ Project/app.py
@@ -0,0 +1,31 @@
+from asyncio import tasks
+from re import I
+from flask import Flask, render_template, url_for, request
+from models.random_output import user_input, word_list, random_output
+
+app = Flask(__name__)
+
+ 
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+
+
+@app.route('/result',methods=['POST', 'GET'])
+
+def result():
+    words = []  
+    usr_input = ""  
+    words.append(request.form['name'])
+    for i in words:
+        usr_input += i 
+    list = word_list(usr_input)
+    task = random_output(list)
+    return render_template('index.html', task = task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
