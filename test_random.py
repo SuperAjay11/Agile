f71338bebe88bd62be2352d1f475090162a9a281
MunibJ <98194546+MunibJ@users.noreply.github.com>
2022-05-19 19:45:26 -0700

    updated random_test.py

--- test_random.py
+++ test_random.py
@@ -1,16 +1,18 @@
-from models.random_output import user_input, word_list, random_output
+from Project.models.random_output import user_input, word_list, random_output
 from unittest.mock import *
 import pytest
 
 
-@patch("builtins.input", side_effect=["one two three"]) 
-def test_example(mock_input): 
-    value1 = input() 
+@patch("builtins.input", side_effect=["one two three"])
+def test_example(mock_input):
+    value1 = input()
     assert value1 == "one two three"
-    
+
+
 def test_list():
     assert word_list("one two three") == ["one", "two", "three"]
 
+
 def test_random():
     random = random_output(["one", "two", "three"])
-    assert random in ["one", "two", "three"] 
+    assert random in ["one", "two", "three"]
